ef6994a236734df3c3f1226aaa15de2750c5b804
zametkikostik <150536839+zametkikostik@users.noreply.github.com>
2026-08-31 00:21:28 +0300

    TOTP copy uses SecureClipboard auto-clear

--- mobile/lib/features/vault/totp_code_widget.dart
+++ mobile/lib/features/vault/totp_code_widget.dart
@@ -2,8 +2,8 @@ import 'dart:async';
 
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
+import '../../core/util/secure_clipboard.dart';
 import '../../core/util/totp.dart';
 
 class TotpCodeWidget extends StatefulWidget {
@@ -71,11 +71,13 @@ class _TotpCodeWidgetState extends State<TotpCodeWidget> {
           ),
           IconButton(
             icon: const Icon(Icons.copy),
-            onPressed: () {
-              Clipboard.setData(ClipboardData(text: _code));
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(content: Text('copied'.tr())),
-              );
+            onPressed: () async {
+              await SecureClipboard.copy(_code);
+              if (context.mounted) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('copied_clears'.tr())),
+                );
+              }
             },
           ),
         ],
